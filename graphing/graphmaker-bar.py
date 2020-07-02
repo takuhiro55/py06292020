a6be475cec64b6c3b6e3d69dde74ef833eb70d2b
Takuhiro <takuhiro55@gmail.com>
2020-07-02 13:40:22 -0400

    To see a graph in lab38 in basic

---
+++ graphing/graphmaker-bar.py
@@ -0,0 +1,39 @@
+#!/
+
+"""
+
+"""
+
+import numpy as np
+import matplotlib
+matplotlib.use('Agg')
+
+import matplotlib.pyplot as plt
+
+def main():
+    N = 4
+    localnetMeans = (20, 35, 30, 35)
+    wanMeans = (25, 32, 34, 20)
+    ind = np.arange(N)
+    width = 0.35
+
+    # Describe where to display p1
+    p1 = plt.bar(ind, localnetMeans, width)
+
+    # Stack p2 on top of p1
+    p2 = plt.bar(ind, wanMeans, width, bottom=localnetMeans)
+
+    # Describe the table metadata
+    plt.ylabel("Length of Outage (mins)")
+    plt.title("2018 Network Summary")
+    plt.xticks(ind, ("Q1", "Q2", "Q3", "Q4"))
+    plt.yticks(np.arange(0, 81, 10))
+    plt.legend((p1[0], p2[0]), ("LAN", "WAN"))
+
+    # Display the graph 
+    # -> Since tmux does not work, I will save it and take it to gitHub
+    # plt.show()
+    plt.savefig("/home/student/mycode/graphing/2018summary.png")
+
+if __name__ == "__main__":
+    main()
